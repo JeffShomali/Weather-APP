7f546c38f47357be61a04add8f7a4b15b1975f3b
Jeff Shomali <jeffshomali@gmail.com>
2016-12-07 20:37:07 -0800

    Adding WeatherMessage and WetaherForm Components to Repo

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -10,6 +10,7 @@ var Nav = React.createClass({
                <Link to="/about" activeClassName="active" activeStyle={{fontWeight: "bold"}}>About</Link>
                <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: "bold"}}>Examples</Link>
           </div>
+
      );
 }
 });
---
+++ app/components/WeatherMessage.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var WeatherMessage = React.createClass({
+     render: function() {
+          return (
+                <h1>Concord is 40 tommorow.</h1>
+          )
+     }
+});
+
+module.exports = WeatherMessage;
