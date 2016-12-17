cdb6ea870de822db3810ab2aa232fafe9375609f
Jeff Shomali <jeffshomali@gmail.com>
2016-12-17 12:30:48 -0800

    Prep repository for Heroku

--- server.js
+++ server.js
@@ -3,10 +3,21 @@ var express = require('express');
 // Creat our App
 var app = express();
 
+// If is local use port 3000 else use env.PORT
+const PORT = proccess.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+     if (req.headers['x-forwarded-proto'] === 'http'){
+          next();
+     }else {
+          res.redirect('http://' + req.hostname + req.url);
+     }
+});
+
 // Tel it which folder wants to serve it
 app.use(express.static('public'));
 
 // Start the server
-app.listen(3000, function(){
-     console.log("Express server is up on port 3000");
+app.listen(PORT, function(){
+     console.log("Express server is up on port " + PORT);
 });
